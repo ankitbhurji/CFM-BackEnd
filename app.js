6849d00a43795bc9de98668883f71fb3e16e2d57
ankitbhurji <bhurji204@gmail.com>
2023-01-25 17:58:28 +0530

    boilerplate server setup

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!'); 
+});
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });

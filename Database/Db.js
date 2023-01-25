aed63f63ad4a294b263693b486f000c53188cc79
ankitbhurji <bhurji204@gmail.com>
2023-01-25 19:19:53 +0530

    added database config and initialzation

---
+++ Database/Db.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+
+
+// const  url = "mongodb://0.0.0.0:27017/cfmDB"
+const  url = process.env.MONGODB_url;
+
+const Connection = () =>{
+    mongoose.connect(url, (err)=>{
+        if(!err){
+            console.log("database is connected")
+        }else{
+            console.log("database is not connected", err)
+        }
+    })
+}
+
+module.exports = Connection;
--- app.js
+++ app.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 app.use(express.json())
-
-
+const CONNECTION = require("./Database/Db");
+CONNECTION();
 
 app.get('/', (req, res) => {
     res.send('Hello World!'); 
 });
+
+app.get('/health', (req, res)=>{
+    res.send({
+        status: "server is healthy", 
+        Time: new Date()
+    })
+})
 
 
 const PORT = process.env.PORT;
